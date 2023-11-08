40a976bb7eb0d535d89ae094090d9283f685efd2
Brian <18603393+brian6932@users.noreply.github.com>
2023-11-08 04:09:16 -0500

    Fix mangled text when piping unicode into rentry

--- rentry.py
+++ rentry.py
@@ -84,15 +84,15 @@ Commands:
   new   create a new entry
   edit  edit an existing entry
   raw   get raw markdown text of an existing entry
-    
+
 Options:
   -h, --help                 show this help message and exit
   -u, --url URL              url for the entry, random if not specified
   -p, --edit-code EDIT-CODE  edit code for the entry, random if not specified
-    
+
 Examples:
   rentry new 'markdown text'               # new entry with random url and edit code
-  rentry new -p pw -u example 'text'       # with custom edit code and url 
+  rentry new -p pw -u example 'text'       # with custom edit code and url
   rentry edit -p pw -u example 'text'      # edit the example entry
   cat FILE | rentry new                    # read from FILE and paste it to rentry
   cat FILE | rentry edit -p pw -u example  # read from FILE and edit the example entry
@@ -102,6 +102,7 @@ Examples:
 
 
 if __name__ == '__main__':
+    sys.stdin.reconfigure(encoding='utf-8')
     try:
         environ.pop('POSIXLY_CORRECT', None)
         opts, args = getopt.gnu_getopt(sys.argv[1:], "hu:p:", ["help", "url=", "edit-code="])
